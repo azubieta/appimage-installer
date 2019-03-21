8d58240a327754e5e3157a87a9bd004ac8820382
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-03-21 16:48:15 +0100

    Improve help

--- src/main.cpp
+++ src/main.cpp
@@ -9,23 +9,32 @@
 #include "commands/RemoveCommand.h"
 
 int main(int argc, char** argv) {
-
     QCoreApplication app(argc, argv);
 
     QCommandLineParser parser;
     parser.addPositionalArgument("command", QCoreApplication::translate("cli-main",
                                                                         "Command to be executed: search | get | list | update | remove"));
+    parser.setApplicationDescription(
+        QCoreApplication::translate("cli-main",
+                                    "Command details\n"
+                                    "   search <query>              list applications available in the store\n"
+                                    "   get <STORE ID>              get the application with the given store id\n"
+                                    "   list                        list applications available on your system\n"
+                                    "   update <APP ID>             update if possible the given application\n"
+                                    "   remove <APP ID>             remove the application from your system"));
+
+    parser.addHelpOption();
     parser.process(app);
 
     QStringList args = parser.positionalArguments();
-    if (args.size() == 0)
+    if (args.empty())
         parser.showHelp(0);
 
     Command* command = nullptr;
     if (args.first() == "search") {
         args.pop_front();
         if (args.empty())
-            qFatal("Missing search query, try:\n\tapp search franz");
+            qFatal("Missing search query. Example:\n\tapp search firefox");
 
         command = new SearchCommand(args.first());
     }
@@ -33,7 +42,7 @@ int main(int argc, char** argv) {
     if (args.first() == "get") {
         args.pop_front();
         if (args.empty())
-            qFatal("Missing application id, try:\n\tapp get franz");
+            qFatal("Missing store id. Example:\n\tapp get 1230");
 
         command = new GetCommand(args.first());
     }
@@ -44,10 +53,13 @@ int main(int argc, char** argv) {
     if (args.first() == "remove") {
         args.pop_front();
         if (args.empty())
-            qFatal("Missing application id, try:\n\tapp remove franz");
+            qFatal("Missing application id. Example:\n\tapp remove firefox");
 
         command = new RemoveCommand(args.first());
     }
+
+    if (args.first() == "update")
+        qFatal("ERROR: Updates aren't supported yet. Use AppImageUpate in the meanwhile.");
 
     if (command) {
         QObject::connect(command, &Command::executionCompleted, &app, &QCoreApplication::quit, Qt::QueuedConnection);
@@ -59,7 +71,7 @@ int main(int argc, char** argv) {
         QMetaObject::invokeMethod(&app, &QCoreApplication::startingUp);
         command->execute();
 
-        return app.exec();
+        return QCoreApplication::exec();
     } else
         parser.showHelp(0);
 }

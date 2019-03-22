2a9e95084b5b443da6b7ff046e30df0f26e2bdf1
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-03-22 13:16:44 +0100

    Fix ecm install script

--- .travis/install_ecm_from_sources.sh
+++ .travis/install_ecm_from_sources.sh
@@ -13,7 +13,7 @@ ECM_BUILD_DIR=/tmp/ecm_build
 git clone https://anongit.kde.org/extra-cmake-modules.git --depth=1 ${ECM_SOURCE_DIR}
 git -C ${ECM_SOURCE_DIR} submodule update --init --recursive;
 
-mkdir -p ECM_BUILD_DIR; cd ${ECM_SOURCE_DIR}
+mkdir -p ${ECM_BUILD_DIR}; cd ${ECM_BUILD_DIR}
 cmake ${ECM_SOURCE_DIR} -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=Release
 make -j`nproc` install
 

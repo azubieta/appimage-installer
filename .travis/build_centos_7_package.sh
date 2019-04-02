02385d68b7e060b2cb064dbad71cf40294210b3f
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-04-02 22:15:41 +0200

    Fix script

--- .travis/build_centos_7_package.sh
+++ .travis/build_centos_7_package.sh
@@ -8,12 +8,12 @@ SOURCES_DIR=`dirname ${SOURCES_DIR}`
 mkdir docker-centos7-build-release
 
 sudo docker build -t build/centos7 ${SOURCES_DIR}/.travis/docker/centos7/
-sudo docker run -v ${PWD}:/source -v ${PWD}/docker-centos7-build-release:/build build/centos7 /bin/bash -c "
-        cmake3 /source
-            -DINSTALL_LIBAPPIMAGE=On
-            -DINSTALL_ATTICA=On
-            -DCMAKE_INSTALL_PREFIX=/usr
-            -DCMAKE_BUILD_TYPE=Release
-            &&
-         make -j`nproc` &&
+sudo docker run -v ${PWD}:/source -v ${PWD}/docker-centos7-build-release:/build build/centos7 /bin/bash -c "\
+        cmake3 /source \
+            -DINSTALL_LIBAPPIMAGE=On\
+            -DINSTALL_ATTICA=On\
+            -DCMAKE_INSTALL_PREFIX=/usr\
+            -DCMAKE_BUILD_TYPE=Release\
+            &&\
+         make -j`nproc` &&\
          cpack3 -G RPM"

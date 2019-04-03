6350ff84d025fc9b8c6e21ff4dc4c899cbedcec6
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-04-03 20:59:01 +0200

    correct dependencies

--- .travis/build_opensuse_leap_package.sh
+++ .travis/build_opensuse_leap_package.sh
@@ -7,7 +7,7 @@ SOURCES_DIR=`dirname ${SOURCES_DIR}`
 
 mkdir docker-centos7-build-release
 
-export OPENSUSE_PACKAGE_REQUIRES="boost-filesystem, libarchive, cairo, librsvg2"
+export OPENSUSE_PACKAGE_REQUIRES="libboost_filesystem1_61_0, libarchive, cairo, librsvg2"
 export OPENSUSE_PACKAGE_PROVIDES="libappimage.so.1.0()(64bit), libKF5Attica.so.5()(64bit)"
 
 sudo docker build -t build/opensuse:leap ${SOURCES_DIR}/.travis/docker/opensuse_leap/

a851ffb64b7dc9a114bbf5110a1553ec51c5112b
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-04-03 22:39:18 +0200

    Rename project to appimamgehub_cli

--- cmake/versioning.cmake
+++ cmake/versioning.cmake
@@ -2,12 +2,12 @@
 # borrowed from AppImageLauncher #
 ##################################
 
-set(AIUT_V_MAJOR 0)
-set(AIUT_V_MINOR 0)
-set(AIUT_V_PATCH 1)
-set(AIUT_V_SUFFIX "")
+set(APPIMAGEHUB_CLI_V_MAJOR 0)
+set(APPIMAGEHUB_CLI_V_MINOR 0)
+set(APPIMAGEHUB_CLI_V_PATCH 1)
+set(APPIMAGEHUB_CLI_V_SUFFIX "")
 
-set(AIUT_VERSION ${AIUT_V_MAJOR}.${AIUT_V_MINOR}.${AIUT_V_PATCH}${V_SUFFIX})
+set(APPIMAGEHUB_CLI_VERSION ${APPIMAGEHUB_CLI_V_MAJOR}.${APPIMAGEHUB_CLI_V_MINOR}.${APPIMAGEHUB_CLI_V_PATCH}${V_SUFFIX})
 
 # check whether git is available
 find_program(GIT git)
@@ -21,7 +21,7 @@ if(NOT GIT STREQUAL "GIT-NOTFOUND")
     execute_process(
         COMMAND git rev-parse --short HEAD
         WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-        OUTPUT_VARIABLE AIUT_GIT_COMMIT
+        OUTPUT_VARIABLE APPIMAGEHUB_CLI_GIT_COMMIT
         OUTPUT_STRIP_TRAILING_WHITESPACE
         ERROR_QUIET
         RESULT_VARIABLE GIT_RESULT
@@ -29,7 +29,7 @@ if(NOT GIT STREQUAL "GIT-NOTFOUND")
 
     if(GIT_RESULT EQUAL 0)
         message(STATUS "Storing git commit ID in cache file")
-        file(WRITE "${GIT_COMMIT_CACHE_FILE}" "${AIUT_GIT_COMMIT}")
+        file(WRITE "${GIT_COMMIT_CACHE_FILE}" "${APPIMAGEHUB_CLI_GIT_COMMIT}")
     endif()
 endif()
 
@@ -40,51 +40,51 @@ if(NOT GIT_RESULT EQUAL 0)
 
     message(WARNING "Failed to gather commit ID via git command, trying to read cache file")
     if(EXISTS "${GIT_COMMIT_CACHE_FILE}")
-        file(READ "${GIT_COMMIT_CACHE_FILE}" AIUT_CACHED_GIT_COMMIT)
-        mark_as_advanced(FORCE AIUT_CACHED_GIT_COMMIT)
-        string(REPLACE "\n" "" AIUT_GIT_COMMIT "${AIUT_CACHED_GIT_COMMIT}")
+        file(READ "${GIT_COMMIT_CACHE_FILE}" APPIMAGEHUB_CLI_CACHED_GIT_COMMIT)
+        mark_as_advanced(FORCE APPIMAGEHUB_CLI_CACHED_GIT_COMMIT)
+        string(REPLACE "\n" "" APPIMAGEHUB_CLI_GIT_COMMIT "${APPIMAGEHUB_CLI_CACHED_GIT_COMMIT}")
     else()
         message(FATAL_ERROR "Could not find git commit cache file, git commit ID not available for versioning")
     endif()
 endif()
 
-if("${AIUT_GIT_COMMIT}" STREQUAL "")
-    message(FATAL_ERROR "Invalid git commit ID: ${AIUT_GIT_COMMIT}")
+if("${APPIMAGEHUB_CLI_GIT_COMMIT}" STREQUAL "")
+    message(FATAL_ERROR "Invalid git commit ID: ${APPIMAGEHUB_CLI_GIT_COMMIT}")
 endif()
 
-message(STATUS "Git commit: ${AIUT_GIT_COMMIT}")
-mark_as_advanced(FORCE AIUT_GIT_COMMIT)
+message(STATUS "Git commit: ${APPIMAGEHUB_CLI_GIT_COMMIT}")
+mark_as_advanced(FORCE APPIMAGEHUB_CLI_GIT_COMMIT)
 
 # add build number based on Travis build number if possible
 if("$ENV{TRAVIS_BUILD_NUMBER}" STREQUAL "")
-    set(AIUT_BUILD_NUMBER "<local dev build>")
+    set(APPIMAGEHUB_CLI_BUILD_NUMBER "<local dev build>")
 else()
-    set(AIUT_BUILD_NUMBER "$ENV{TRAVIS_BUILD_NUMBER}")
+    set(APPIMAGEHUB_CLI_BUILD_NUMBER "$ENV{TRAVIS_BUILD_NUMBER}")
 endif()
 
 # get current date
 execute_process(
     COMMAND env LC_ALL=C date -u "+%Y-%m-%d %H:%M:%S %Z"
-    OUTPUT_VARIABLE AIUT_BUILD_DATE
+    OUTPUT_VARIABLE APPIMAGEHUB_CLI_BUILD_DATE
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
 # get current date, short form
 execute_process(
-    COMMAND env LC_ALL=C git show -s --format=%ci ${AIUT_GIT_COMMIT}
-    OUTPUT_VARIABLE AIUT_GIT_COMMIT_DATE
+    COMMAND env LC_ALL=C git show -s --format=%ci ${APPIMAGEHUB_CLI_GIT_COMMIT}
+    OUTPUT_VARIABLE APPIMAGEHUB_CLI_GIT_COMMIT_DATE
     OUTPUT_STRIP_TRAILING_WHITESPACE
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
 )
 execute_process(
-    COMMAND env LC_ALL=C date -u "+%Y%m%d" -d ${AIUT_GIT_COMMIT_DATE}
-    OUTPUT_VARIABLE AIUT_GIT_COMMIT_DATE_SHORT
+    COMMAND env LC_ALL=C date -u "+%Y%m%d" -d ${APPIMAGEHUB_CLI_GIT_COMMIT_DATE}
+    OUTPUT_VARIABLE APPIMAGEHUB_CLI_GIT_COMMIT_DATE_SHORT
     OUTPUT_STRIP_TRAILING_WHITESPACE
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
 )
 
-add_definitions(-DAIUT_VERSION="${AIUT_VERSION}")
-add_definitions(-DAIUT_GIT_COMMIT="${AIUT_GIT_COMMIT}")
-add_definitions(-DAIUT_BUILD_NUMBER="${AIUT_BUILD_NUMBER}")
-add_definitions(-DAIUT_BUILD_DATE="${AIUT_BUILD_DATE}")
+add_definitions(-DAPPIMAGEHUB_CLI_VERSION="${APPIMAGEHUB_CLI_VERSION}")
+add_definitions(-DAPPIMAGEHUB_CLI_GIT_COMMIT="${APPIMAGEHUB_CLI_GIT_COMMIT}")
+add_definitions(-DAPPIMAGEHUB_CLI_BUILD_NUMBER="${APPIMAGEHUB_CLI_BUILD_NUMBER}")
+add_definitions(-DAPPIMAGEHUB_CLI_BUILD_DATE="${APPIMAGEHUB_CLI_BUILD_DATE}")
 

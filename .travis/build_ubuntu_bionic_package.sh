2b664342f34e85c10ad253e05bca2815d85ae759
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-04-03 16:07:04 +0200

    Build and install attica as part of the ubuntu bionic package

--- .travis/build_ubuntu_bionic_package.sh
+++ .travis/build_ubuntu_bionic_package.sh
@@ -9,4 +9,4 @@ mkdir docker-build-release
 
 sudo docker build -t build/ubuntu:bionic ${SOURCES_DIR}/.travis/docker/ubuntu_bionic/
 sudo docker run -v ${PWD}:/source -v ${PWD}/docker-build-release:/build build/ubuntu:bionic \
-    /bin/bash -c "cmake -DINSTALL_LIBAPPIMAGE=On -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release /source && make -j`nproc` && cpack deb"
+    /bin/bash -c "cmake -DINSTALL_LIBAPPIMAGE=On -DINSTALL_ATTICA=On -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release /source && make -j`nproc` && cpack deb"

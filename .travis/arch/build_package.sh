e988c053c4b77cf7c80af3dd881f90528091b559
Alexis López Zubieta <azubieta@users.noreply.github.com>
2019-04-04 21:07:24 +0200

    Arch package (#18)
    Build arch package on travis

---
+++ .travis/arch/build_package.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Find the right sources dir path
+SOURCES_DIR=`realpath $0`
+SOURCES_DIR=`dirname ${SOURCES_DIR}`
+
+mkdir docker-build-release
+chmod a+rwx docker-build-release
+
+sudo docker build -t build/arch ${SOURCES_DIR}
+sudo docker run -v ${PWD}:/sources -v ${PWD}/docker-build-release:/build build/arch \
+    /bin/bash -c "cp /sources/.travis/arch/PKGBUILD . && makepkg"

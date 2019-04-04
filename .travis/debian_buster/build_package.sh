0e15c8f7cfe19d71a4297ddb7fe4b88192d78076
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-04-04 21:40:58 +0200

    arrange debian buster package

--- .travis/debian_buster/build_package.sh
+++ .travis/debian_buster/build_package.sh
@@ -3,10 +3,9 @@
 # Find the right sources dir path
 SOURCES_DIR=`realpath $0`
 SOURCES_DIR=`dirname ${SOURCES_DIR}`
-SOURCES_DIR=`dirname ${SOURCES_DIR}`
 
 mkdir docker-build-release
 
-sudo docker build -t build/debian:buster ${SOURCES_DIR}/.travis/docker/debian_buster/
+sudo docker build -t build/debian:buster ${SOURCES_DIR}
 sudo docker run -v ${PWD}:/source -v ${PWD}/docker-build-release:/build build/debian:buster \
     /bin/bash -c "cmake -DINSTALL_LIBAPPIMAGE=On -DINSTALL_ATTICA=On -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release /source && make -j`nproc` && cpack -G DEB -G DEB -R 0.1.0-debian-buster"

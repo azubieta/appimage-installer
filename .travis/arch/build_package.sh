1bc654a2f7b56bbc69d8d596e3554f06064a344d
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-04-04 20:09:54 +0200

    Improve dockerfile

--- .travis/arch/build_package.sh
+++ .travis/arch/build_package.sh
@@ -5,5 +5,5 @@ SOURCES_DIR=`realpath $0`
 SOURCES_DIR=`dirname ${SOURCES_DIR}`
 
 sudo docker build -t build/arch ${SOURCES_DIR}
-sudo docker run -v ${PWD}:/sources build/arch \
+sudo docker run -v ${PWD}:/sources build/arch -v ${PWD}/docker-build-relaese:/build build/arch \
     /bin/bash -c "cp /sources/.travis/arch/PKGBUILD . && makepkg"

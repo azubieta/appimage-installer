3103dacaba275caaed33b2eb7baaffe9c1539e78
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-04-04 19:53:17 +0200

    Improve dockerfile

--- .travis/arch/build_package.sh
+++ .travis/arch/build_package.sh
@@ -5,8 +5,6 @@ SOURCES_DIR=`realpath $0`
 SOURCES_DIR=`dirname ${SOURCES_DIR}`
 SOURCES_DIR=`dirname ${SOURCES_DIR}`
 
-
-
 sudo docker build -t build/arch ${SOURCES_DIR}/.travis/docker/arch/
-sudo docker run -v ${PWD}:/source -v ${PWD}/docker-build-release:/build build/arch \
-    /bin/bash -c "cmake -DINSTALL_LIBAPPIMAGE=On -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release /source && make -j`nproc`"
+sudo docker run -v ${PWD}:/source build/arch \
+    /bin/bash -c "cp /sources/.travis/arch/PKGBUILD . && makepkg"

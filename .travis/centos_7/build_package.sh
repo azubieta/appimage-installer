265eb742ce858337d993e8e38946f2f7b685f02f
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-04-05 10:53:16 -0400

    Arrange centos 7 package

--- .travis/centos_7/build_package.sh
+++ .travis/centos_7/build_package.sh
@@ -3,15 +3,13 @@
 # Find the right sources dir path
 SOURCES_DIR=`realpath $0`
 SOURCES_DIR=`dirname ${SOURCES_DIR}`
-SOURCES_DIR=`dirname ${SOURCES_DIR}`
 
 mkdir docker-centos7-build-release
 
 export CENTOS7_PACKAGE_REQUIRES="boost-filesystem, libarchive, cairo, librsvg2"
 export CENTOS7_PACKAGE_PROVIDES="libappimage.so.1.0()(64bit), libKF5Attica.so.5()(64bit)"
 
-sudo docker build -t build/centos7 ${SOURCES_DIR}/.travis/docker/centos_7/
-
+sudo docker build -t build/centos7 ${SOURCES_DIR}
 sudo docker run -v ${PWD}:/source -v ${PWD}/docker-build-release:/build build/centos7 /bin/bash -c "\
         cmake3 /source \
             -DINSTALL_LIBAPPIMAGE=On\

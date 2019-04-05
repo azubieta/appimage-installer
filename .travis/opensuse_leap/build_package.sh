45ed6241ac0c99b3cc473be5dd2e806cc39d6d92
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-04-05 10:55:01 -0400

    Arrange opensuse leap package

--- .travis/opensuse_leap/build_package.sh
+++ .travis/opensuse_leap/build_package.sh
@@ -3,14 +3,13 @@
 # Find the right sources dir path
 SOURCES_DIR=`realpath $0`
 SOURCES_DIR=`dirname ${SOURCES_DIR}`
-SOURCES_DIR=`dirname ${SOURCES_DIR}`
 
 mkdir docker-centos7-build-release
 
 export OPENSUSE_PACKAGE_REQUIRES="libboost_filesystem1_61_0, libarchive13, cairo, librsvg2"
 export OPENSUSE_PACKAGE_PROVIDES="libappimage.so.1.0()(64bit), libKF5Attica.so.5()(64bit)"
 
-sudo docker build -t build/opensuse:leap ${SOURCES_DIR}/.travis/docker/opensuse_leap/
+sudo docker build -t build/opensuse:leap ${SOURCES_DIR}
 sudo docker run -v ${PWD}:/source -v ${PWD}/docker-build-release:/build build/opensuse:leap /bin/bash -c "\
         cmake /source \
             -DINSTALL_LIBAPPIMAGE=On\

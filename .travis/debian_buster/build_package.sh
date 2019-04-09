bf443bd22b7357f619183ef6479f49d767f06c45
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-04-08 21:33:20 -0400

    Bump version to 0.1.2

--- .travis/debian_buster/build_package.sh
+++ .travis/debian_buster/build_package.sh
@@ -8,4 +8,4 @@ mkdir docker-build-release
 
 sudo docker build -t build/debian:buster ${SOURCES_DIR}
 sudo docker run -v ${PWD}:/source -v ${PWD}/docker-build-release:/build build/debian:buster \
-    /bin/bash -c "cmake -DINSTALL_LIBAPPIMAGE=On -DINSTALL_ATTICA=On -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release /source && make -j`nproc` && cpack -G DEB -G DEB -R 0.1.1-debian-buster"
+    /bin/bash -c "cmake -DINSTALL_LIBAPPIMAGE=On -DINSTALL_ATTICA=On -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release /source && make -j`nproc` && cpack -G DEB -G DEB -R 0.1.2-debian-buster"
